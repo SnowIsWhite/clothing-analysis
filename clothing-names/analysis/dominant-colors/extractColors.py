14ce301e1e420e2d1dff78cee80e5428dfccf806
SnowIsWhite <jibae617@gmail.com>
2018-07-24 01:43:18 +0900

    google vision api

---
+++ clothing-names/analysis/dominant-colors/extractColors.py
@@ -0,0 +1,29 @@
+import io
+import os
+
+from google.cloud import vision
+from google.cloud.vision import types
+
+path = './sample.jpg'
+def detect_properties(path):
+    client = vision.ImageAnnotatorClient()
+
+    with io.open(path, 'rb') as image_file:
+        content = image_file.read()
+
+    image = types.Image(content=content)
+
+    response = client.image_properties(image=image)
+    props = response.image_properties_annotation
+    print('Properties:')
+
+    for color in props.dominant_colors.colors:
+        print('fraction: {}'.format(color.pixel_fraction))
+        print('\tr: {}'.format(color.color.red))
+        print('\tg: {}'.format(color.color.green))
+        print('\tb: {}'.format(color.color.blue))
+        print('\ta: {}'.format(color.color.alpha))
+
+ 
+if __name__ == "__main__":
+    detect_properties(path)
